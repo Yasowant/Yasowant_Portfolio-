456c6b5c7508be4942fb761199ae9b86090dd3df
Yasowant Nayak <129403851+Yasowant@users.noreply.github.com>
2023-05-29 14:52:15 +0530

    Add files via upload

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./About.css";
+import { Type } from "./Type";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import PhoneIcon from "@material-ui/icons/Phone";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import { Facebook } from "@material-ui/icons";
+// import PublicIcon from "@material-ui/icons/Public";
+import { Introduction } from "./Introduction";
+import { Timeline } from "./Timeline";
+import { Techstacks } from "./Techstacks";
+// import Button from "@mui/material/Button";
+export const About = () => {
+  return (
+    <>
+      <div className="about center">
+        <h1 data-aos="fade-right" className="mobileHead">
+          Hello, I am <span className="about__name">Yasowant Nayak</span>
+        </h1>
+        <Type />
+        <p className="about__desc" data-aos="fade-right">
+          Passionate and creative Full Stack Developer having proficiency in
+          MERN Stack. Attired with a variety of tools & technologies and keen to
+          learn new one.
+        </p>
+        <div className="about__contact center">
+          <a
+            href="https://github.com/Yasowant"
+            aria-label="github"
+            target="_blank"
+            rel="noreferrer"
+            className="link link--icon"
+          >
+            <GitHubIcon />
+          </a>
+          <a
+            href="mailto:yasowant1998@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="mail"
+            className="link link--icon"
+          >
+            <EmailIcon />
+          </a>
+          <a
+            href="tel:+919354617055"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="phone"
+            className="link link--icon"
+          >
+            <PhoneIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yasowant-nayak-154968269/"
+            aria-label="linkedin"
+            className="link link--icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            href="https://www.facebook.com/yasowant.nayak/"
+            aria-label="linkedin"
+            className="link link--icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            < Facebook/>
+          </a>
+
+
+          <a
+            href="https://twitter.com/_yasowant"
+            aria-label="twitter"
+            className="link link--icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TwitterIcon />
+          </a>
+        </div>
+
+        <button
+          className="btnResume"
+          onClick={() => {
+            window.open(
+              "https://drive.google.com/file/d/1CyNCgeYZn0Og-592L0j3c7Wy54zkKNrb/view?usp=share_link"
+            );
+          }}
+        >
+          Resume
+        </button>
+      </div>
+      <Introduction />
+      <Timeline />
+      <section id="#skills">
+        <Techstacks />
+      </section>
+    </>
+  );
+};
